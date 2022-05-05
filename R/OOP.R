b025e244d70c9e434016b47f65086c5ad4412856
PiotrTymoszuk <80723424+PiotrTymoszuk@users.noreply.github.com>
2022-05-05 11:47:52 +0200

    Inital commit
    Functions and S3 API

---
+++ R/OOP.R
@@ -0,0 +1,155 @@
+# S3 OOP interface.
+
+# Constructors ------
+
+#' Create a xena database object.
+#'
+#' @description Creates a XENA object on the top of a data frame.
+#' @param x a data frame.
+#' @param ... extra arguments, currently none.
+#' @return a xena database which inherits most of its methods from the
+#' data.frame class.
+#' @export
+
+  xena <- function(x, ...) {
+
+    if(!is.data.frame(x)) {
+
+      stop('Please provide a data frame.', call. = FALSE)
+
+    }
+
+    structure(x,
+              class = c('xena',
+                        'tbl_df',
+                        'tbl',
+                        'data.frame'))
+
+  }
+
+# Class checker -----
+
+#' Cehcks for the XENA class.
+#'
+#' @description Checks of the object is an instance of the XENA class.
+#' @param x an object.
+#' @return a logical value.
+#' @export
+
+  is_xena <- function(x) {
+
+    any(class(x) == 'xena')
+
+  }
+
+# Methods -----
+
+#' Query a local XENA database.
+#'
+#' @description Searches for samples carrying the specified mutation.
+#' Lists all the samples with a mutation of the given gene, chromosome, with
+#' the effect provided or mutation ID. Searches directly via the key value or
+#' with a regular expression. The option 'detailed' returns a complete subset
+#' of the xena_mut object. Otherwise, only the sample ids and vaf value are
+#' returned.
+#' @param x a XENA object.
+#' @param key name of the database field to search in. Possible values are:
+#' 'gene' (gene with the mutation), 'chrom' (chromosome), 'effect' (effect of
+#' the mutation), 'mutation_id' (mutation ID).
+#' @param value a key value or a vector for the direct search.
+#' @param regex a regular expression to scan the key, ignored if value is
+#' provided.
+#' @param unique_samples logical, should duplicated samples be excluded from
+#' the output?
+#' @param join logical, should the output be merged into a single data frame by
+#' the sample ID?
+#' @param detailed logical, if TRUE a XENA object being a subset of the input
+#' is returned.
+#' @return a data frame or a XENA object, if detailed = TRUE.
+#' @import gseaTools
+#' @export search.xena
+#' @export
+
+  search.xena <- function(x,
+                              key = c('gene', 'chrom', 'effect', 'mutation_id'),
+                              value = NULL,
+                              regex = NULL,
+                              unique_samples = FALSE,
+                              join = FALSE,
+                              detailed = FALSE) {
+
+    ## entry control
+
+    stopifnot(xena::is_xena(x))
+
+    key <- match.arg(key[1], c('gene', 'chrom', 'effect', 'mutation_id'))
+
+    if(!is.null(value)) regex <- NULL
+
+    stopifnot(is.logical(unique_samples))
+    stopifnot(is.logical(join))
+    stopifnot(is.logical(detailed))
+
+    ## benchmarking
+
+    start_time <- Sys.time()
+    message(paste('Checking mutations for', length(value), key, 'values'))
+    on.exit(message(paste('Elapsed:', Sys.time() - start_time)))
+
+    ## quering
+
+    if(!is.null(regex)) {
+
+      x <- dplyr::filter(x, stringi::stri_detect(.data[[key]], regex = regex))
+
+    }
+
+    if(!is.null(value)) {
+
+      x <- dplyr::filter(x, .data[[key]] %in% value)
+
+    }
+
+    ## searching the data base
+
+    if(!detailed) {
+
+      query <- plyr::dlply(x, key, function(x) x[c('sample_id', 'dna_vaf')])
+
+      query <- purrr::map2(query,
+                           names(query),
+                           ~rlang::set_names(.x, c('sample_id', .y)))
+
+    } else {
+
+      query <- plyr::dlply(x, key)
+
+    }
+
+    if(unique_samples) {
+
+      query <- purrr::map(query,
+                          ~dplyr::filter(.x, !duplicated(.x)))
+
+
+    }
+
+    if(!join){
+
+      purrr::map(query, tibble::as_tibble)
+
+    } else {
+
+      query <- purrr::reduce(query, dplyr::full_join, by = 'sample_id')
+
+      query <- tibble::as_tibble(query)
+
+      purrr::map_dfc(query, ~ifelse(is.na(.x), 0, .x))
+
+    }
+
+  }
+
+
+
+
---
+++ R/functions.R
@@ -0,0 +1,202 @@
+# Loading and management of a XENA database.
+
+# Loading -----
+
+#' Load a XENA data base.
+#'
+#' @description Loads a XENA database from the disc and converts it to a
+#' tidyverse - compatible format.
+#' @param path path to the XENA database file; accepts .tar or .gz archives.
+#' @return a XENA object, with a data frame interface.
+#' @export
+
+  load_xena <- function(path) {
+
+    xena_db <- readr::read_tsv(path)
+
+    xena_db <- dplyr::mutate(xena_db,
+                             sample_id = Sample_ID,
+                             mutation_id = paste(chrom,
+                                                 start,
+                                                 end,
+                                                 ref,
+                                                 alt,
+                                                 sep = '_'))
+
+    xena_db <- dplyr::select(xena_db, - Sample_ID)
+
+    xena::xena(xena_db)
+
+  }
+
+# Mutation counting ------
+
+#' Count mutations per sample.
+#'
+#' @description Counts all mutations per sample provided in the XENA class
+#' database.
+#' @param xena_db a XENA database object.
+#' @param ... extra arguments, currently none.
+#' @return a data frame listing the samples and the number of mutations per
+#' sample.
+#' @export
+
+  count_mutations <- function(xena_db, ...) {
+
+    ## entry control
+
+    if(!xena::is_xena(xena_db)) {
+
+      stop('Please provide a valid XENA database object.', call. = FALSE)
+
+    }
+
+    ## counting
+
+    xena_counts <- plyr::dlply(xena_db,
+                               'sample_id',
+                               dplyr::filter,
+                               !duplicated(mutation_id))
+
+    purrr::map2_dfr(xena_counts,
+                    names(xena_counts),
+                    ~tibble::tibble(sample_id = .y,
+                                    mut_count = nrow(.x)))
+
+  }
+
+# Mutation table/matrix ------
+
+#' List mutated genes.
+#'
+#' @description Creates a matrix or table with the mutated genes for each
+#' sample (0: wt, 1: a mutation listed in the database). Specificity towards
+#' specific mutations (e.g. missense ones) and their quantity (Variant Allele
+#' Frequency) can be acheived by simple filtering of the input xena_bd object
+#' (inherits the transformation interface from data.frame).
+#' @param xena_db a XENA database object.
+#' @param as_matrix logical, should the output be in a matrix form?
+#' @param sparse logical or NULL, specifying if the result should be sparse or
+#' not. By default, it is made sparse when more than half of the entries are 0.
+#' @param .parallel logical, should the operation be run in parallel? Runs via
+#' the foreach backend, the user need to register the process prior to calling
+#' the function.
+#' If FALSE, a data.frame is returned (slower).
+#' @param ... extra arguments, currently none.
+#' @return a (sparse) matrix or a data frame.
+#' @export
+
+  tab_mutations <- function(xena_db,
+                            as_matrix = TRUE,
+                            sparse = NULL,
+                            .parallel = FALSE, ...) {
+
+    ## entry control
+
+    if(!xena::is_xena(xena_db)) {
+
+      stop('Please provide a valid XENA database object.', call. = FALSE)
+
+    }
+
+    stopifnot(is.logical(as_matrix))
+    stopifnot(is.logical(.parallel))
+
+    ## benchmarking
+
+    start_time <- Sys.time()
+    message(paste('Mutation tabulation for n =', nrow(xena_db), 'records.'))
+    on.exit(message(paste('Elapsed:', Sys.time() - start_time)))
+
+    ## re-shaping the table
+
+    mut_tbl <- plyr::dlply(xena_db,
+                           'gene',
+                           .parallel = .parallel,
+                           dplyr::filter,
+                           !duplicated(sample_id))
+
+    ## the simple reduction with merging won't work because of the computation
+    ## time. Instead, an iterative approach using a matrix
+
+    av_samples <- unique(xena_db$sample_id)
+    av_genes <- names(mut_tbl)
+
+    mut_mtx <- Matrix::Matrix(0,
+                              nrow = length(av_samples),
+                              ncol = length(av_genes),
+                              dimnames = list(av_samples, av_genes),
+                              sparse = sparse)
+
+    for(i in names(mut_tbl)) {
+
+      mut_record <- mut_tbl[[i]]
+
+      mut_mtx[mut_record$sample_id, i] <- 1
+
+    }
+
+    if(as_matrix) return(mut_mtx)
+
+    mut_mtx <- as.matrix(mut_mtx)
+
+    mut_mtx <- tibble::rownames_to_column(data.frame(mut_mtx), 'sample_id')
+
+    tibble::as_tibble(mut_mtx)
+
+  }
+
+# Mutation frequency -----
+
+#' Gen frequency of mutated genes.
+#'
+#' @description Calculates frequencies of the mutated genes in the database.
+#' @param xena_db a XENA database object.
+#' @param .parallel logical, should the computation be run in parallel?
+#' @param ... extra arguments, currently none.
+#' @return a data frame with the frequency and percent of mutations.
+#' @export
+
+  freq_mutations <- function(xena_db, .parallel = FALSE, ...) {
+
+    ## entry control
+
+    stopifnot(xena::is_xena(xena_db))
+
+    stopifnot(is.logical(.parallel))
+
+    ## mutation matrix
+
+    mut_mat <- xena::tab_mutations(xena_db,
+                                   as_matrix = TRUE,
+                                   sparse = TRUE,
+                                   .parallel = FALSE)
+
+    av_genes <- colnames(mut_mat)
+
+    ## frequencies
+
+    start_time <- Sys.time()
+    message('Mutation frequencies')
+    on.exit(paste('Elapsed:', Sys.time() - start_time))
+
+    if(.parallel) {
+
+      future::plan('multisession')
+
+      freqs <- furrr::future_map_dbl(av_genes,
+                                     ~mean(mut_mat[, .x], na.rm = TRUE))
+
+      future::plan('sequential')
+
+    } else {
+
+      freqs <- purrr::map_dbl(av_genes, ~mean(mut_mat[, .x], na.rm = TRUE))
+
+    }
+
+    tibble::tibble(gene = av_genes,
+                   frequency = freqs,
+                   percent = freqs * 100)
+
+  }
